51c364f630160ef930fb9c5d12d8ecb5e3bc0d89
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-08-01 07:59:06 -0700

    Html structure correct

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Data } from "./Data";
 
-const Search = () => {
+const App = () => {
   const [store, setStore] = useState(Data);
-  return <div className="container">
-    <h1>Search Filter App</h1>
-    <input type="text" placeholder="Search..." />
-    {
-      store.map((cur) => {
-        return (
-          <div className="iteam-list">
-            <p>{cur.name}</p>
-            <p></p>
-            <img />
+  return (
+    <div className="container">
+      <h1 className="heading">Search Filter App</h1>
 
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search-input"
+      />
+
+      <div className="card-wrapper">
+        {store.map((cur, index) => (
+          <div className="item-card" key={index}>
+            <img src={cur.img} alt={cur.name} className="item-img" />
+            <p className="item-name">{cur.name}</p>
+            <p className="item-brand">{cur.brand}</p>
           </div>
-        );
-      })
-    }
-  </div>;
+        ))}
+      </div>
+    </div>
+
+  );
 };
-export default Search;
+export default App;
--- src/Data.jsx
+++ src/Data.jsx
@@ -3,6 +3,7 @@ import food2 from "./Images/foods2.jpg";
 import food3 from "./Images/foods3.jpg";
 import food4 from "./Images/foods4.webp";
 import food5 from "./Images/foods5.jpg";
+import food6 from "./Images/foods6.jpeg";
 
 
 export const Data = [
