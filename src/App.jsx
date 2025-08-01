d915d6451787060cbba81b6f4f21ca3a77edd050
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-08-01 07:39:18 -0700

    Implement search functionality by mapping over data to display items

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import { Data } from "./Data";
 
 const Search = () => {
+  const [store, setStore] = useState(Data);
   return <div className="container">
     <h1>Search Filter App</h1>
     <input type="text" placeholder="Search..." />
+    {
+      store.map((cur) => {
+        return (
+          <div className="iteam-list">
+            <p>{cur.name}</p>
 
+          </div>
+        );
+      })
+    }
   </div>;
-}
+};
 export default Search;
