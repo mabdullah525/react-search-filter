4c1b6e470e155246b07f7a06e460d39f3577ba76
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-08-17 08:25:04 -0700

    All done

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,42 +1,43 @@
-import food1 from "./Images/foods1.jpg";
-import food2 from "./Images/foods2.jpg";
-import food3 from "./Images/foods3.jpg";
-import food4 from "./Images/foods4.webp";
-import food5 from "./Images/foods5.jpg";
-import food6 from "./Images/foods6.webp";
-
+import food1 from "./images/foods1.jpg";
+import food2 from "./images/foods2.jpg";
+import food3 from "./images/foods3.jpg";
+import food4 from "./images/foods4.webp";
+import food5 from "./images/foods5.jpg";
+import food6 from "./images/foods6.webp";
 
 export const Data = [
-    {
-        name:"Pizza",
-        brand:"Dominose",
-        img:food1
-    },
-    {
-        name:"Burger",
-        brand:"KFC",
-        img:food2
-    },
-    {
-        name:"Shwarma",
-        brand:"Al-Baik",
-        img:food3
-    },
-    {
-        name:"Biryani",
-        brand:"Nawab",
-        img:food4
-    },
-    {
-        name:"Paneer",
-        brand:"Shahi",
-        img:food5
-    },
-    {
-        name:"Red Velvet",
-        brand:"Layers",
-        img:food6
-    },
-]
+  {
+    name: "Pizza",
+    brand: "Dominose",
+    img: food1,
+  },
+  {
+    name: "Burger",
+    brand: "KFC",
+    img: food2,
+  },
+  {
+    name: "Shwarma",
+    brand: "Al-Baik",
+    img: food3,
+  },
+  {
+    name: "Biryani",
+    brand: "Nawab",
+    img: food4,
+  },
+  {
+    name: "Paneer",
+    brand: "Shahi",
+    img: food5,
+  },
+  {
+    name: "Red Velvet",
+    brand: "Layers",
+    img: food6,
+  },
+];
+
+
 
  
