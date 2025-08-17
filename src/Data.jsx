f812c15597db1b522ed94a3e9008bb903a6a551d
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-08-17 09:23:11 -0700

    images issue

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,9 +1,10 @@
-import food1 from "./images/foods1.jpg";
+import food1 from "./images/foods1.JPG"; // capital extension
 import food2 from "./images/foods2.jpg";
 import food3 from "./images/foods3.jpg";
 import food4 from "./images/foods4.webp";
 import food5 from "./images/foods5.jpg";
 import food6 from "./images/foods6.webp";
+
 
 export const Data = [
   {
